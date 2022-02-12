4e039646b967e7337423abd4b5da156f34d9540d
moritz <theincxption@gmail.com>
2022-02-12 11:35:24 +0100

    Use '@ngneat/until-destroy' to automatically unsubscribe from observables

--- client/src/app/room/room.component.ts
+++ client/src/app/room/room.component.ts
@@ -1,18 +1,16 @@
-import { Component, OnDestroy, OnInit } from "@angular/core"
+import { Component, OnInit } from "@angular/core"
 import { ActivatedRoute } from "@angular/router"
-import { firstValueFrom, Subscription } from "rxjs"
 import { ChatService } from "../services/chat.service"
+import { UntilDestroy, untilDestroyed } from "@ngneat/until-destroy"
 
+@UntilDestroy()
 @Component({
    selector: "app-room",
    templateUrl: "./room.component.html",
    styleUrls: ["./room.component.scss"]
 })
-export class RoomComponent implements OnInit, OnDestroy {
+export class RoomComponent implements OnInit {
    roomId: string = ""
-
-   $chatSubscription?: Subscription
-   $userSubscription?: Subscription
 
    users: number = 0
    message: string = ""
@@ -21,21 +19,19 @@ export class RoomComponent implements OnInit, OnDestroy {
    constructor(private route: ActivatedRoute, private chatService: ChatService) {}
 
    async ngOnInit() {
-      const params = await firstValueFrom(this.route.params)
-      this.roomId = params["id"]
+      this.route.queryParams
+         .pipe(untilDestroyed(this))
+         .subscribe(params => (this.roomId = params["id"]))
 
-      this.$chatSubscription = this.chatService.getChat().subscribe(message => {
-         this.messages.push(message)
-      })
+      this.chatService
+         .getChat()
+         .pipe(untilDestroyed(this))
+         .subscribe(message => this.messages.push(message))
 
-      this.$userSubscription = this.chatService.getUsers().subscribe(users => {
-         this.users = users
-      })
-   }
-
-   ngOnDestroy(): void {
-      this.$chatSubscription?.unsubscribe()
-      this.$userSubscription?.unsubscribe()
+      this.chatService
+         .getUsers()
+         .pipe(untilDestroyed(this))
+         .subscribe(users => (this.users = users))
    }
 
    sendChat() {
